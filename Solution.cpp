9982d6b81c7f1e41eaa5342d30e836cfd7aae8f3
Mikael Tulldahl <mikael.tulldahl@live.se>
2019-05-13 21:25:01 +0200

    Implemented path planning for exploration

--- Solution.cpp
+++ Solution.cpp
@@ -8,6 +8,12 @@
 class API;
 
 class Solution {
+#define DIRS 4
+#define FORWARD 0
+#define LEFT 1
+#define BACKWARD 2
+#define RIGHT 3
+
 #define MAP_SIZE 30
 #define UNSEEN 0
 #define EMPTY 1
@@ -15,34 +21,63 @@ class Solution {
 #define WALL 3
 #define ENEMY 4
 
-  int heading = 0;
+#define STAND_STILL 0
+#define MOVE_FORWARD 1
+#define MOVE_BACKWARD 2
+#define TURN_RIGHT 3
+#define TURN_LEFT 4
+
+  int robotHeading = 0;
   int robotX = MAP_SIZE / 2;
   int robotY = MAP_SIZE / 2;
   int map[MAP_SIZE][MAP_SIZE] =
       {};  // possible values: UNSEEN, EMPTY, UNKNOWN, WALL
   char canvas[MAP_SIZE][MAP_SIZE] = {};
   int enemyCount;
-  int enemyState[4];  // possible values: UNKNOWN, WALL, ENEMY
-  int enemyX[4];      // -1: no enemy for certain, 0-MAP_SIZE: coord for enemy
-  int enemyY[4];      // -1: no enemy for certain, 0-MAP_SIZE: coord for enemy
+  int enemyState[DIRS];  // possible values: UNKNOWN, WALL, ENEMY
+  int enemyX[DIRS];  // -1: no enemy for certain, 0-MAP_SIZE: coord for enemy
+  int enemyY[DIRS];  // -1: no enemy for certain, 0-MAP_SIZE: coord for enemy
  public:
   Solution() {
     // If you need initialization code, you can write it here!
     // Do not remove.
   }
 
-  int wrap(int i) { return (i % 4 + 4) % 4; }
+  int wrap(int i) { return (i % DIRS + DIRS) % DIRS; }
+
+  void moveRobot(int action) {
+    switch (action) {
+      case STAND_STILL:
+        // do nothing
+        break;
+      case MOVE_FORWARD:
+        moveForward();
+        break;
+      case MOVE_BACKWARD:
+        moveBackward();
+        break;
+      case TURN_RIGHT:
+        turnRight();
+        break;
+      case TURN_LEFT:
+        turnLeft();
+        break;
+      default:
+        // do nothing
+        break;
+    }
+  }
 
   void turnRight() {
     API::turnRight();
-    heading--;
-    heading = wrap(heading);
+    robotHeading--;
+    robotHeading = wrap(robotHeading);
   }
 
   void turnLeft() {
     API::turnLeft();
-    heading++;
-    heading = wrap(heading);
+    robotHeading++;
+    robotHeading = wrap(robotHeading);
   }
 
   void turnToDirLocal(int dirLocal) {
@@ -63,18 +98,22 @@ class Solution {
   }
 
   void moveForward() {
-    API::moveForward();
-    map[robotX][robotY] = EMPTY;          // previous pos is now empty space
-    getXY(heading, 1, &robotX, &robotY);  // update position
+    if (API::lidarFront() > 1) {
+      API::moveForward();
+      addXY(robotHeading, 1, &robotX, &robotY);  // update position
+    } else {
+      cout << "TRIED TO GO INTO A WALL" << endl;
+    }
   }
 
-  bool blockF() { return API::lidarFront() <= 1; }
-
-  bool blockB() { return API::lidarBack() <= 1; }
-
-  bool blockR() { return API::lidarRight() <= 1; }
-
-  bool blockL() { return API::lidarLeft() <= 1; }
+  void moveBackward() {
+    if (API::lidarBack() > 1) {
+      API::moveBackward();
+      addXY(robotHeading, -1, &robotX, &robotY);  // update position
+    } else {
+      cout << "TRIED TO GO INTO A WALL" << endl;
+    }
+  }
 
   bool coinToss(float probabilityTrue) {
     int temp = rand() % 100000;
@@ -121,7 +160,7 @@ class Solution {
 
     int nearestEnemyDirLocal = 0;
     int nearestEnemyDist = MAP_SIZE;
-    for (int dirLocal = 0; dirLocal < 4; dirLocal++) {
+    for (int dirLocal = 0; dirLocal < DIRS; dirLocal++) {
       if (enemyState[dirLocal] == ENEMY) {
         int tempDist = lidarLocal(dirLocal) + dir2TurnCount(dirLocal);
         if (tempDist < nearestEnemyDist) {
@@ -133,7 +172,7 @@ class Solution {
 
     int nearestUnknownDirLocal = 0;
     int nearestUnknownDist = MAP_SIZE;
-    for (int dirLocal = 0; dirLocal < 4; dirLocal++) {
+    for (int dirLocal = 0; dirLocal < DIRS; dirLocal++) {
       if (enemyState[dirLocal] == UNKNOWN) {
         int tempDist = lidarLocal(dirLocal) + dir2TurnCount(dirLocal);
         if (tempDist < nearestUnknownDist) {
@@ -143,27 +182,137 @@ class Solution {
       }
     }
 
-    cout << "Heading: " << heading << endl;
+    cout << "Heading: " << robotHeading << endl;
     cout << "Fuel: " << API::currentFuel() << endl;
     cout << "nearestEnemyDirLocal: " << nearestEnemyDirLocal
          << " nearestEnemyDist: " << nearestEnemyDist << endl;
     cout << "nearestUnknownDirLocal: " << nearestUnknownDirLocal
          << " nearestUnknownDist: " << nearestUnknownDist << endl;
 
-    if (nearestEnemyDist < MAP_SIZE &&
-        (nearestEnemyDist <= nearestUnknownDist || nearestUnknownDist > 4)) {
-      if (nearestEnemyDirLocal == 0) {
-        API::fireCannon();
-        cout << "Action: Fire" << endl;
+    bool hasThreats = (nearestEnemyDist < MAP_SIZE) || (nearestUnknownDist < 3);
+    bool prioritizeEnemies = (nearestEnemyDist <= (nearestUnknownDist + 2));
+    if (hasThreats) {
+      if (prioritizeEnemies) {
+        if (nearestEnemyDirLocal == 0) {  // enemy in front
+          if (nearestEnemyDist <= 3) {    // close enough to shoot
+            API::fireCannon();
+            cout << "Action: Fire" << endl;
+          } else {
+            moveForward();
+            cout << "Action: Enemy ahead" << endl;
+          }
+        } else {
+          turnToDirLocal(nearestEnemyDirLocal);
+          cout << "Action: Turn to enemy" << endl;
+        }
       } else {
-        turnToDirLocal(nearestEnemyDirLocal);
-        cout << "Action: Turn to enemy" << endl;
+        turnToDirLocal(nearestUnknownDirLocal);
+        cout << "Action: Turn to unknown" << endl;
       }
-    } else if (nearestUnknownDist <= 6) {
-      turnToDirLocal(nearestUnknownDirLocal);
-      cout << "Action: Turn to unknown" << endl;
     } else {
-      moveRandom();
+#define MOVES_NUM 15
+#define CANDIDATES_NUM 50
+      int pathCandidates[MOVES_NUM][CANDIDATES_NUM];  // 5 moves, 10 candidates
+      int bestScore = 0;
+      int bestFirstMove = STAND_STILL;
+      int candidateMap[MAP_SIZE][MAP_SIZE];
+
+      for (int i = 0; i < CANDIDATES_NUM; i++) {
+        // reset candidate map
+        for (int n = 0; n < MAP_SIZE; n++) {    // x
+          for (int m = 0; m < MAP_SIZE; m++) {  // y
+            candidateMap[n][m] = map[n][m];
+          }
+        }
+        int candidateX = robotX;
+        int candidateY = robotY;
+        int candidateHeading = robotHeading;
+        int candidateFirstMove;
+        int candidateScore = 0;
+        for (int j = 0; j < MOVES_NUM; j++) {
+          // execute move
+          int move = chooseMove(candidateHeading, candidateX, candidateY);
+          if (j == 0) {
+            candidateFirstMove = move;
+          }
+          switch (move) {
+            case MOVE_FORWARD:
+              addXY(candidateHeading, 1, &candidateX, &candidateY);
+              break;
+            case TURN_RIGHT:
+              candidateHeading--;
+              candidateHeading = wrap(candidateHeading);
+              break;
+            case TURN_LEFT:
+              candidateHeading++;
+              candidateHeading = wrap(candidateHeading);
+              break;
+          }
+          // evaulate position
+          for (int dirLocal = 0; dirLocal < DIRS; dirLocal++) {
+            bool forward = dirLocal == FORWARD;
+            // seek with lidar
+            int lidarX = candidateX;
+            int lidarY = candidateY;
+            int dist = 0;
+            bool done = false;
+            while (!done) {
+              addXY(dirLocal2dirWorld(candidateHeading, dirLocal), 1, &lidarX,
+                    &lidarY);
+              dist++;
+              if (!isInMap(lidarX, lidarY)) {
+                done = true;
+              } else {
+                switch (candidateMap[lidarX][lidarY]) {
+                  case UNSEEN:
+                    if (forward) {
+                      candidateMap[lidarX][lidarY] = WALL;
+                      candidateScore += 10;
+                    } else {
+                      candidateMap[lidarX][lidarY] = UNKNOWN;
+                      candidateScore += 7;
+                      if (dist == 1) {
+                        candidateScore -=
+                            6;  // penalty for going close to unknown stuff
+                      }
+                    }
+                    done = true;
+                    break;
+                  case EMPTY:
+                    break;
+                  case UNKNOWN:
+                    if (forward) {
+                      candidateMap[lidarX][lidarY] = WALL;
+                      candidateScore += 3;
+                    }
+                    if (dist == 1) {
+                      candidateScore -=
+                          1;  // penalty for going close to unknown stuff
+                    }
+                    done = true;
+                    break;
+                    break;
+                  case WALL:
+                    done = true;
+                    break;
+                  default:  // error
+                    break;
+                }
+              }
+            }
+          }
+          if (candidateScore > bestScore) {
+            bestScore = candidateScore;
+            bestFirstMove = candidateFirstMove;
+          }
+        }
+      }
+      if (bestScore > 0) {
+        moveRobot(bestFirstMove);
+      } else {
+        moveRobot(chooseMove(robotHeading, robotX, robotY));
+      }
+
       cout << "Action: Random walk" << endl;
     }
   }
@@ -171,22 +320,27 @@ class Solution {
   void updateMap() {
     // possible values for map: UNSEEN, EMPTY, UNKNOWN, WALL
     // possible values for enemyState:        UNKNOWN, WALL, ENEMY
+    map[robotX][robotY] = EMPTY;  // robot pos is always empty space
     int measX;
     int measY;
     enemyCount = 0;
-    for (int dirWorld = 0; dirWorld < 4; dirWorld++) {
+    for (int dirWorld = 0; dirWorld < DIRS; dirWorld++) {
       int range = lidarWorld(dirWorld);
       for (int i = 1; i < range; i++) {  // fill empty space
-        getXY(dirWorld, i, &measX, &measY);
+        measX = robotX;
+        measY = robotY;
+        addXY(dirWorld, i, &measX, &measY);
         if (isInMap(measX, measY)) {
           map[measX][measY] = EMPTY;
         }
       }
-      getXY(dirWorld, range, &measX, &measY);
+      measX = robotX;
+      measY = robotY;
+      addXY(dirWorld, range, &measX, &measY);
       if (isInMap(measX, measY)) {
-        if (dirWorld == heading) {      // forward lidar
-          if (API::identifyTarget()) {  // enemy only temporarily occupies
-                                        // empty space, therefore empty space
+        if (dirWorld == robotHeading) {  // forward lidar
+          if (API::identifyTarget()) {   // enemy only temporarily occupies
+                                         // empty space, therefore empty space
             map[measX][measY] = EMPTY;
           } else {  // wall
             map[measX][measY] = WALL;
@@ -207,7 +361,7 @@ class Solution {
           }
         }
       }
-      int dirLocal = dirWorld2dirLocal(dirWorld);
+      int dirLocal = dirWorld2dirLocal(robotHeading, dirWorld);
       switch (map[measX][measY]) {
         case UNKNOWN:
           enemyState[dirLocal] = UNKNOWN;
@@ -268,7 +422,7 @@ class Solution {
   void plotRobotOnCanvas() {
     if (isInMap(robotX, robotY)) {
       char c;
-      switch (heading) {
+      switch (robotHeading) {
         case 0:
           c = '>';
           break;
@@ -291,7 +445,7 @@ class Solution {
   }
 
   void plotEnemiesOnCanvas() {
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < DIRS; i++) {
       if (isInMap(enemyX[i], enemyY[i])) {
         canvas[enemyX[i]][enemyY[i]] = 'O';
       }
@@ -319,11 +473,9 @@ class Solution {
     cout << endl;
   }
 
-  // will return x and y coordinate relative robot
-  void getXY(int dirWorld, int dist, int* xPtr, int* yPtr) {
-    *xPtr = robotX;
-    *yPtr = robotY;
-    switch (dirWorld) {
+  // will add dist in dir to xy coordinate
+  void addXY(int dir, int dist, int* xPtr, int* yPtr) {
+    switch (dir) {
       case 0:
         *xPtr += dist;
         break;
@@ -344,7 +496,7 @@ class Solution {
 
   // returns range for lidar in dirWorld direction
   int lidarWorld(int dirWorld) {
-    return lidarLocal(dirWorld2dirLocal(dirWorld));
+    return lidarLocal(dirWorld2dirLocal(robotHeading, dirWorld));
   }
 
   // returns range for lidar in dirWorld direction
@@ -364,41 +516,61 @@ class Solution {
     }
   }
 
-  int dirWorld2dirLocal(int dirWorld) { return wrap(dirWorld - heading); }
+  // dirLocal = dirWorld - heading
+  int dirWorld2dirLocal(int heading, int dirWorld) {
+    return wrap(dirWorld - heading);
+  }
 
-  void moveRandom() {
-    if (blockF() && blockB() && blockR() && blockL()) {  // nowhere to move
-      turnRight();
-    } else if (blockF() && blockB() && blockL()) {  // only free right
-      turnRight();
-    } else if (blockF() && blockB() && blockR()) {  // only free left
-      turnLeft();
-    } else if (blockF() && blockR() && blockL()) {  // only free backwards
-      turnRight();
-    } else {  // multiple free directions
-      int temp = rand() % 8;
-      switch (temp) {
-        case 0:
-          if (blockL()) {
-            moveRandom();
-          } else {
-            turnLeft();
-          }
+  // dirWorld = dirLocal + heading
+  int dirLocal2dirWorld(int heading, int dirLocal) {
+    return wrap(dirLocal + heading);
+  }
+
+  bool freeToMove(int dirLocal, int heading, int x, int y) {
+    addXY(dirLocal2dirWorld(heading, dirLocal), 1, &x, &y);
+    if (isInMap(x, y)) {
+      switch (map[x][y]) {
+        case UNSEEN:
+        case EMPTY:
+          return true;
           break;
-        case 1:
-          if (blockR()) {
-            moveRandom();
-          } else {
-            turnRight();
-          }
+        case UNKNOWN:
+        case WALL:
+          return false;
           break;
         default:
-          if (blockF()) {
-            moveRandom();
-          } else {
-            moveForward();
-          }
+          cout << "incorrect map element in  in blockF" << endl;
+          return false;
+          break;
       }
+    } else {
+      return false;
+    }
+  }
+
+  int chooseMove(int heading, int x, int y) {
+    int temp = rand() % 6;
+    switch (temp) {
+      case 0:
+        return TURN_LEFT;
+        break;
+      case 1:
+        return TURN_RIGHT;
+        break;
+      case 2:
+        if (freeToMove(BACKWARD, heading, x, y)) {
+          return MOVE_BACKWARD;
+        } else {
+          return chooseMove(heading, x, y);
+        }
+        break;
+      default:
+        if (freeToMove(FORWARD, heading, x, y)) {
+          return MOVE_FORWARD;
+        } else {
+          return chooseMove(heading, x, y);
+        }
+        break;
     }
   }
 };
